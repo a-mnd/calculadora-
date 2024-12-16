675d1a3b53d62ccc2e0f6b2b3123df2882d7b9bc
Amanda Petronieri Feo <163210289+a-mnd@users.noreply.github.com>
2024-12-16 10:23:28 -0300

    Add files via upload

---
+++ calc.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="calc.css" />
+    <title>Calculadora</title>
+  </head>
+  <body>
+    <div class="calculadora">
+      <div class="resultado">
+        <p>
+          10 x 5
+          <span> 50 </span>
+        </p>
+      </div>
+      <div class="teclas">
+        <button class="cinzaclaro">C</button>
+        <button class="cinzaclaro">+/-</button>
+        <button class="cinzaclaro">%</button>
+        <button class="roxo">/</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button class="roxo">X</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button class="roxo">-</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button class="roxo">+</button>
+        <button class="cols-2">0</button>
+        <button>.</button>
+        <button class="roxo">=</button>
+      </div>
+    </div>
+  </body>
+</html>
